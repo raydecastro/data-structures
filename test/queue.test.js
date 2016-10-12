930a02864fc32a58e4d0d3cb65bace8beb7384c9
raydecastro <captray108@gmail.com>
2016-10-11 17:17:46 -0700

    feature. added isEmpty().

--- test/queue.test.js
+++ test/queue.test.js
@@ -9,4 +9,10 @@ describe("Queue", () => {
 
     expect(queue).to.be.defined;
   });
+
+  it("shall have ability to check if it's empty", () => {
+    let queue = new Queue();
+
+    expect(queue.isEmpty()).to.be.true;
+  });
 });
